2cf28f07a8a8d452802c2bf6f0b4cd71611b1049
nureex <nureexamahmud@gmail.com>
2024-06-09 17:59:02 +0100

    J objects scopes closures

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,4 +1,5 @@
-#!/usr/bin/node
-let list = require('./100-data').list;
+const list = require('./100-data').list;
+
 console.log(list);
-console.log(list.map((element, index) => element * index));
+const newList = list.map((element, index) => element * index);
+console.log(newList);
